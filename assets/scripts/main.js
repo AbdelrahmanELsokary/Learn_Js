abfb4e032bacd1ada9b18cd244684f684082063e
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-01-03 14:31:18 +0200

    Add: JavaScript String Methods

---
+++ assets/scripts/main.js
@@ -0,0 +1,31 @@
+
+/*
+  !JavaScript String Methods
+ * .length        
+ * .trim()        
+ * .trimStart()   
+ * .trimEnd()     
+ * .toUpperCase() 
+ * .toLowerCase() 
+ * .charAt()      
+ * .reapet()      
+ * .concat()     
+ * .split()
+ */
+
+/*
+ * index  Start From 0
+ * length Start From 1
+ */
+
+// let myName = "Abdelrahman Mahmoud";
+// let lastName = "Elsokary";
+// console.log(myName.trim()); //Removes the leading and trailing white space and line terminator characters from a string.
+// console.log(myName.trimStart()); //Removes the leading white space and line terminator characters from a string.
+// console.log(myName.trimEnd());  //Removes the trailing white space and line terminator characters from a string.
+// console.log(myName.toUpperCase()); //Converts all the alphabetic characters in a string to uppercase.
+// console.log(myName.toLowerCase()); //Converts all the alphabetic characters in a string to lowercase.
+// console.log(myName.charAt(10)); //Returns the character at the specified index , start from zero
+// console.log(myName.repeat(77)); //Returns a String value that is made from count copies appended together. If count is 0, the empty string is returned.
+// console.log(myName.concat(lastName)); // To combine string or arrays with each other.
+// console.log(myName.split(" ")); //Splits strings into an array of parts based on a given separator (Delimiter).
